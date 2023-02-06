ff7f31c2d17aa9e3b666ad7699d791d4f6140751
david <david.ezequiel.perez@gmail.com>
2023-02-06 10:34:51 -0300

    NodeJS project first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { parse } = require('csv-parse')
+const fs = require('fs')
+
+const isHabitable = []
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6
+}
+
+fs.createReadStream('kepler-data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            isHabitable.push(data)
+        }
+    })
+    .on('error', (err) =>{
+        console.log(err)
+    })
+    .on('end', () =>{
+        console.log(isHabitable.map((planet) => {
+            return planet['kepler_name']
+        })),
+        console.log(`${isHabitable.length} habitable planets found!`)
+    })
+
